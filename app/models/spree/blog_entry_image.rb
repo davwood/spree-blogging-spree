0e4e65e55081822c3f2ad03f98a0da02d5f6977a
davewood <davwood_1@yahoo.com.au>
2014-03-25 10:58:10 +1100

    updated

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,3 +1,16 @@
 class Spree::BlogEntryImage < Spree::Image
-  has_attached_file :attachment 
+  has_attached_file :attachment, 
+   :styles => { :big => "500>", :medium => "300x300>"},
+   s3_credentials: {
+		  access_key_id: ENV["S3_KEY"],
+		  secret_access_key: ENV["S3_SECRET"],
+		  bucket: "rungazella-dev",
+		},
+	storage: :s3,
+	s3_headers:     { "Cache-Control" => "max-age=31557600" },
+	s3_protocol:    "https",
+	bucket:         "rungazella-dev",
+    path: "/:class/:attachment/:id/:style/:basename.:extension",
+	default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
+	default_style: "medium"
 end
